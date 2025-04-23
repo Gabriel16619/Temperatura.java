95d5a9f776cdbb86e866d8b4d76f106152fe7b8e
GABRIEL CAVALCANTE DOS SANTOS <25132825@CFP127ALUNO.LOCAL>
2025-04-23 16:15:12 -0300

    começo da criação do conversor de temperatura

---
+++ src/br/dev/gabriel/temperatura/Temperatura.java
@@ -0,0 +1,5 @@
+package br.dev.gabriel.temperatura;
+
+public class Temperatura {
+
+}
---
+++ src/br/dev/gabriel/temperatura/TemperaturaApp.java
@@ -0,0 +1,10 @@
+package br.dev.gabriel.temperatura;
+
+public class TemperaturaApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
